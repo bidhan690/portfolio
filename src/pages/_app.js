a8c15df55e498dd54fba336205534fb11a084c51
Bidhan Niroula <116049938+bidhan690@users.noreply.github.com>
2023-03-19 16:41:47 +0000

    removed pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,10 @@
 import "@/styles/globals.css";
 import Layout from "@/components/header/Layout";
-import { AnimatePresence, motion } from "framer-motion";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <AnimatePresence mode="wait">
-        <motion.div
-          initial={{ y: "-100px" }}
-          animate={{ y: 0 }}
-          exit={{ x: "1000px" }}
-          transition={{ duration: 0.3 }}
-        >
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
+      <Component {...pageProps} />
     </Layout>
   );
 }
