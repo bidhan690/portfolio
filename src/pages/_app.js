ecaab1b9405472786298aff41fc505befd41267d
Bidhan Niroula <116049938+bidhan690@users.noreply.github.com>
2023-03-23 16:54:12 +0545

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,23 @@
 import "@/styles/globals.css";
 import Layout from "@/components/header/Layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <>
+      <Head>
+        <title>Bidhan | Full-Stack Developer</title>
+        <meta
+          name="description"
+          content="Hello, My name is Bidhan Niroula. I am from Nepal. This is my NextJS Portfolio!"
+        />
+
+        <link href="/assets/bidhan.ico" rel="icon" type="image/x-icon" />
+      </Head>
+      <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
+  
   );
 }
